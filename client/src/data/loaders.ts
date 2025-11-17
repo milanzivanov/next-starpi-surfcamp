210b507c493f97a24df694896efbe9edd056d4ae
milan <milanzivanov@users.noreply.github.com>
2025-11-17 12:57:48 +0100

    experience page

--- client/src/data/loaders.ts
+++ client/src/data/loaders.ts
@@ -36,7 +36,7 @@ const homePageQuery = qs.stringify({
 
 export async function getHomePage() {
   const path = "/api/home-page";
-  const BASE_URL = await getStrapiURL();
+  const BASE_URL = getStrapiURL();
 
   const url = new URL(path, BASE_URL);
   url.search = homePageQuery;
@@ -44,4 +44,51 @@ export async function getHomePage() {
   return await fetchAPI(url.href, {
     method: "GET"
   });
+}
+
+const pageBySlugQuery = (slug: string) =>
+  qs.stringify({
+    filters: {
+      slug: {
+        $eq: slug
+      }
+    },
+    populate: {
+      blocks: {
+        on: {
+          "blocks.hero-section": {
+            populate: {
+              image: {
+                fields: ["url", "alternativeText"]
+              },
+              logo: {
+                populate: {
+                  image: {
+                    fields: ["url", "alternativeText"]
+                  }
+                }
+              },
+              cta: true
+            }
+          },
+          "blocks.info-block": {
+            populate: {
+              image: {
+                fields: ["url", "alternativeText"]
+              },
+              cta: true
+            }
+          }
+        }
+      }
+    }
+  });
+
+export async function getPageBySlug(slug: string) {
+  const path = "/api/pages";
+  const BASE_URL = getStrapiURL();
+
+  const url = new URL(path, BASE_URL);
+  url.search = pageBySlugQuery(slug);
+  return await fetchAPI(url.href, { method: "GET" });
 }
