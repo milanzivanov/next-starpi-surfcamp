b24ef29d6d2f8200e399bcc218106b818e2d10b1
milan <milanzivanov@users.noreply.github.com>
2025-11-25 16:52:27 +0100

    Building Single Blog Detail Page part 1

---
+++ client/src/app/blog/[slug]/page.tsx
@@ -0,0 +1,57 @@
+import type { ArticleProps, Block } from "@/types";
+import { notFound } from "next/navigation";
+import { formatDate } from "@/utils/format-date";
+import { getContentBySlug } from "@/data/loaders";
+
+import { HeroSection } from "@/components/blocks/HeroSection";
+
+interface PageProps {
+  params: Promise<{ slug: string }>;
+}
+
+async function loader(slug: string) {
+  const { data } = await getContentBySlug(slug, "/api/articles");
+  const article = data[0];
+  if (!article) throw notFound();
+  return { article: article as ArticleProps, blocks: article?.blocks };
+}
+
+interface ArticleOverviewProps {
+  headline: string;
+  description: string;
+}
+
+function ArticleOverview({
+  headline,
+  description
+}: Readonly<ArticleOverviewProps>) {
+  return (
+    <div className="article-overview">
+      <div className="article-overview__info">
+        <h3 className="article-overview__headline">{headline}</h3>
+        <p className="article-overview__description">{description}</p>
+      </div>
+    </div>
+  );
+}
+export default async function SingleBlogRoute({ params }: PageProps) {
+  const slug = (await params).slug;
+  const { article, blocks } = await loader(slug);
+  const { title, author, publishedAt, description, image } = article;
+
+  return (
+    <div>
+      <HeroSection
+        id={article.id}
+        heading={title}
+        theme="orange"
+        image={image}
+        author={author}
+        publishedAt={formatDate(publishedAt)}
+        darken={true}
+      />
+
+      <ArticleOverview headline={title} description={description} />
+    </div>
+  );
+}
